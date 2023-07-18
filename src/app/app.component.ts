db967981076738dffe99fc10eccb428482663af8
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-18 14:50:51 +0200

    Logout implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,10 @@ export class AppComponent implements OnInit {
   inAuthPage: boolean = false;
   currentPage: string = '';
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+    ) {
   }
 
   ngOnInit(): void {
@@ -24,5 +28,13 @@ export class AppComponent implements OnInit {
       }
       
     });
+  }
+
+  logout() {
+    this.authService.logout()
+    .then(response => {
+      this.router.navigate(['/login']);
+    })
+    .catch(error => console.log(error));
   }
 }
